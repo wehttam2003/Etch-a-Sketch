101d95871e05fbe25e8ab2673c036f4c4dcabfcf
Matthew <mdav121503@gmail.com>
2024-01-22 16:06:27 -0600

    add proper grid

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const padContainer = document.querySelector(".pad-container");
-console.log(padContainer)
+console.log(padContainer);
 
-for (let i = 0; i < 32; i++){
-    let pad = document.createElement("div");
-    pad.classList.add("pad");
-    padContainer.appendChild(pad);
+let rowAmount = 16;
+let columnAmount = 16;
+
+for (let i = 0; i < columnAmount; i++){
+    let padRow = document.createElement("div");
+    padRow.classList.add("pad-row");
+    for (let i = 0; i < rowAmount; i++){
+        let pad = document.createElement("div");
+        pad.classList.add("pad");
+        padRow.appendChild(pad);
+    }
+    padContainer.appendChild(padRow);
 }
