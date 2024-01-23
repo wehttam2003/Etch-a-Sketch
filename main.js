4b6dbc99f1a4e6b86bf5569e446557190473e533
Matthew <mdav121503@gmail.com>
2024-01-23 07:59:21 -0600

    add generatePads function

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const padContainer = document.querySelector(".pad-container");
 console.log(padContainer);
 
-let rowAmount = 16;
-let columnAmount = 16;
-
-for (let i = 0; i < columnAmount; i++){
-    let padRow = document.createElement("div");
-    padRow.classList.add("pad-row");
-    for (let i = 0; i < rowAmount; i++){
-        let pad = document.createElement("div");
-        pad.classList.add("pad");
-        pad.addEventListener("mouseover", () => pad.style.backgroundColor="black")
-
-        padRow.appendChild(pad);
+// TO-DO
+//place the nested for loops in a function that takes single param (padAmount)
+function generatePads(padAmount){
+    for (let i = 0; i < padAmount; i++){
+        let padRow = document.createElement("div");
+        padRow.classList.add("pad-row");
+        
+        for (let i = 0; i < padAmount; i++){
+            let pad = document.createElement("div");
+            pad.classList.add("pad");
+            pad.addEventListener("mouseover", () => pad.style.backgroundColor="black")
+    
+            padRow.appendChild(pad);
+        }
+    
+        padContainer.appendChild(padRow);
     }
-    padContainer.appendChild(padRow);
 }
+
+//create a function to clear the pads
+//create a button that is detached display
+//when the button is pressed promt for number of pads and store it in a variable
+
+generatePads(16);
